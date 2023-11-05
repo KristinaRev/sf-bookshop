ed5fde08a0a4bf2fcd18544c40dc90217622509a
Kristina <yarevyakinakristina@gmail.com>
2023-11-05 13:00:57 +0300

    feat: add style-loader, css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,13 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                use: ['style-loader', 'css-loader'],
+                test: /\.css$/
+            }
+        ]
     }
 }
