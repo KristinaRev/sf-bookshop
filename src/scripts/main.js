136bfd37d6e03c13239057e51e14f24518df3d00
KrisKipper <revyakina@spiks.ru>
2023-11-10 13:05:22 +0300

    feat: fetch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,9 +15,19 @@ window.addEventListener('load', () => {
                 clickable: true,
             },
             autoplay: {
-                delay: 3000,
+                delay: 5000,
             },
         });
+
+        const booksApi = 'https://www.googleapis.com/books/v1/volumes?q="subject:Business"&key=AIzaSyBJze3ZHSv24m5POOMQO0TpJ7SibtMK17s&printType=books&startIndex=0&maxResults=6&langRestrict=en';
+
+        fetch(booksApi)
+            .then(response => response.json())
+
+            .then(data => {
+                console.log(data)
+            })
+            .catch(error => console.error('Ошибка при запросе:', error))
 
     }
 })
